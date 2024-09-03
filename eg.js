9d76190149d3011479c439bbb717b97e266de1bf
aromal-nkm <aromalts2002@gmail.com>
2024-09-03 20:36:29 +0400

    firstcommit

---
+++ eg.js
@@ -0,0 +1,16 @@
+// trim
+var str='         welcome to js';
+console.log(str.trim())
+// slice
+var strr='there are 40 students in FSD at ICATK';
+console.log(strr.slice(8,18));
+// sub
+var strg='welcome to javascript advanced';
+console.log(strg.substring(3,8))
+// map
+
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map(function(num) {
+  return num * 2;
+});
+console.log(doubledNumbers); 
